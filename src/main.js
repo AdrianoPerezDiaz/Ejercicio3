10fc35fc6e738ede937c218875531d9777aeaf2e
Adriano <el-chato1980@hotmail.com>
2024-01-12 16:27:49 +0100

    primer commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+console.log("Hello from main");
+console.log(`
+    Ejercicio3 la invitacion a la cena,
+        Tienes un grupo de 6 amigos y quieres invitarlos a cenar por tu cumpleaños,
+        1-Sólo puedes permitirte invitar a las bebidas, ya que estás un poco ajustado de dinero,
+        2-Tienes un ticket de cena que cuesta 120 € y en el que ya se incluyen las bebidas por un valor de 18 €,
+        3-Calcula cuánto tendría que pagar cada comensal para dividir los costos de manera equitativa,
+        Resultado: 120-18,
+`)
+console.log(120-18)
