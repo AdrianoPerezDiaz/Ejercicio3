2e1856bba316cfab1e4e451236f882df5c5b5696
Adriano <el-chato1980@hotmail.com>
2024-01-18 17:49:46 +0100

    rectificado

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ console.log(`
         1-Sólo puedes permitirte invitar a las bebidas, ya que estás un poco ajustado de dinero,
         2-Tienes un ticket de cena que cuesta 120 € y en el que ya se incluyen las bebidas por un valor de 18 €,
         3-Calcula cuánto tendría que pagar cada comensal para dividir los costos de manera equitativa,
-        Resultado: 120-18,
+        Resultado: (120-18)/7, 7 para incluirme a mi mismo.
 `)
-console.log(120-18)
+console.log((120-18)/7)
